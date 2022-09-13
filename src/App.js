146f694057df0f749b5a087664d6dc2ad9f30c36
DeniSplash <96882650+DeniSplash@users.noreply.github.com>
2022-09-13 21:48:55 +0300

    finish

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.scss';
+
+function App() {
+
+  const text = "Тестовый текст"
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Мой первый React App
+        <Message text={text} />
+      </header>
+    </div>
+  );
+}
+
+const Message = ({ text }) => {
+  return (
+    <div className="Message">
+      <h3>{text}</h3>
+    </div>
+  )
+}
+
+export default App;
